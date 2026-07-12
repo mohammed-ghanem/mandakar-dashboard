c5a365d932ecbf58ab2bc3ea36ed6eb671805f2d
mohammed-ghanem <mohammed.ghanem240@gmail.com>
2026-07-13 01:49:21 +0300

    fixVercel

--- types/roles.ts
+++ types/roles.ts
@@ -23,25 +23,3 @@ export type { Role, Permission } from "@/store/roles/types";
 
 
 
-
-// // types/roles.ts
-
-//   export type Role = {
-//     id: number;
-//     name?: string;
-//     name_ar?: string;
-//     name_en?: string;
-//     slug: string;
-//     is_active?: number ;
-//     created_at: string;
-//     permissions: PermissionGroup[];
-//   };
-  
-//   export type RolesResponse = {
-//     status: number;
-//     message: string;
-//     data: {
-//       data: Role[];
-//     };
-//   };
-  
